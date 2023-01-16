2527ca9c6c0e7fa239c7399895f3aa9f8e55bccd
Rapahael Kim <rageworx@gmail.com>
2023-01-16 17:47:29 +0900

    Rewriting codes in progress.

--- res/resource.h
+++ res/resource.h
@@ -1,2 +1,2 @@
-#define APP_VERSION                             2,2,1,8
-#define APP_VERSION_STR                         "2.2.1.8"
+#define APP_VERSION                             2,2,1,9
+#define APP_VERSION_STR                         "2.2.1.9"
